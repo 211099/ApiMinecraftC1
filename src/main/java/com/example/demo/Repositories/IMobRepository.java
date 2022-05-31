5c949baa372ce8729b57396331b123923cf1e5f2
211099 <211099@ids.upchiapas.edu.mx>
2022-05-31 05:30:25 -0500

    Finished IMobRepository

--- src/main/java/com/example/demo/Repositories/IMobRepository.java
+++ src/main/java/com/example/demo/Repositories/IMobRepository.java
@@ -1,4 +1,17 @@
 package com.example.demo.Repositories;
 
-public interface IMobRepository {
+import com.example.demo.Entities.Mob;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IMobRepository extends JpaRepository<Mob, Long> {
+    List<Mob> findAll();
+    Mob findAllById(int id);
+    Mob save(Mob mod);
+    @Override
+    void delete(Mob mob);
+
 }
