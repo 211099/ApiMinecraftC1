c0764f305c177b97d4afdb50dafa1366ff71a39b
211099 <211099@ids.upchiapas.edu.mx>
2022-05-31 05:59:48 -0500

    add method Get in MobController and add  list in Mobservices

--- src/main/java/com/example/demo/Controllers/MobController.java
+++ src/main/java/com/example/demo/Controllers/MobController.java
@@ -1,4 +1,25 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Entities.Mob;
+import com.example.demo.Services.MobServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("mob")
 public class MobController {
+
+    @Autowired
+    private MobServices services;
+
+    @GetMapping
+    List<Mob> getList(){
+        return services.getList();
+    }
+
+
 }
--- src/main/java/com/example/demo/Services/MobServices.java
+++ src/main/java/com/example/demo/Services/MobServices.java
@@ -1,4 +1,17 @@
 package com.example.demo.Services;
 
+import com.example.demo.Entities.Mob;
+import com.example.demo.Repositories.IMobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MobServices {
+
+    @Autowired
+    private IMobRepository crudService;
+
+    public List<Mob> getList(){return  crudService.findAll(); }
 }
