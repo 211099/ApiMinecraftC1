34f6d6d9d0be9ca63efbf4c0d9b5ed1651809891
Luis Antonio Martinez Marroquin <211105@ids.upchiapas.edu.mx>
2022-05-31 05:57:20 -0500

    completed Entities_Item, ItemRespository

--- src/main/java/com/example/demo/Repositories/IItemRepository.java
+++ src/main/java/com/example/demo/Repositories/IItemRepository.java
@@ -1,4 +1,6 @@
 package com.example.demo.Repositories;
 
-public interface IItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IItemRepository extends JpaRepository {
 }
